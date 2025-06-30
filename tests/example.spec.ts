39c02ea558e4916f9f1781a0fd95ab4e85339723
Povilas Balanda <povilas.balanda@zenitech.co.uk>
2025-06-30 13:22:30 +0300

    Initial commit

---
+++ tests/example.spec.ts
@@ -0,0 +1,27 @@
+import { test, expect } from "@playwright/test";
+import { AutomateItControllerApiFactory } from "automateITApi";
+
+const automateITFactory = AutomateItControllerApiFactory(
+  undefined,
+  "http://localhost:8080"
+);
+
+test("has title", async ({ page }) => {
+  await page.goto("https://playwright.dev/");
+  const response = await automateITFactory.getAutomateITPhrase("123");
+  console.log(response.data);
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+});
+
+test("get started link", async ({ page }) => {
+  await page.goto("https://playwright.dev/");
+
+  // Click the get started link.
+  await page.getByRole("link", { name: "Get started" }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(
+    page.getByRole("heading", { name: "Installation" })
+  ).toBeVisible();
+});
