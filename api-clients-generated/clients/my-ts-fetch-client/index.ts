debafb0382f60efd7dd055cf146d564ef27936d0
Ramon Aseniero <raseniero@gmail.com>
2025-07-28 15:46:30 -0700

    Update .gitignore, add TypeScript client files, and configure API client

---
+++ api-clients-generated/clients/my-ts-fetch-client/index.ts
@@ -0,0 +1,20 @@
+import { JobsApi, Configuration } from 'jairojobsapi';
+
+try {
+    const configuration = new Configuration({
+        apiKey: 'test-api-key-123'
+    });
+    
+    const jobsApi = new JobsApi(configuration);
+    const result = await jobsApi.searchAndListJobs({
+        q: 'developer',
+        location: 'remote',
+        page: 1,
+        limit: 10,
+    });
+
+    console.log(result);
+
+}   catch(error) {
+    console.log(error);
+}
